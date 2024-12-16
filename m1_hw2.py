ed0756918f41e7a5aa808135429822fb90de0934
TurBoO0 <127961357+TurBoO0@users.noreply.github.com>
2024-12-16 18:54:10 +0300

    Add files via upload

---
+++ m1_hw2.py
@@ -0,0 +1,5 @@
+col_hw = 12
+col_hour = 1.5
+nazvanie = 'Python'
+time_for_quest = (col_hour * 60) / col_hw
+print(f'Курс: {nazvanie},\nВсего задач: {col_hw}, \nЗатрачено часов:{col_hour}, \nСреднее время выполнения задания: {time_for_quest}мин')
